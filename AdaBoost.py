252f905532bfe726be1b08ec9ea1ba90c62b1d3c
zhourongchang <497447111@qq.com>
2023-12-29 18:19:46 +0800

    AdaBoost

---
+++ AdaBoost.py
@@ -0,0 +1,69 @@
+from cgi import test
+from re import S
+from tkinter import Y
+from sklearn.datasets import load_breast_cancer,load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+import numpy as np
+
+
+class AdaBoost:
+
+    def __init__(self, X, y, n_estimator) -> None:
+        self.X = X
+        self.y = y
+        self.n_estimator = n_estimator
+        self.models = []
+        self.alphas = []
+        self.label = None
+
+    def fit(self):
+        n_sample, n_feature = self.X.shape
+        self.label = np.unique(self.y)
+        # 初始化权重
+        D = np.ones(n_sample) / n_sample
+
+        for _ in range(self.n_estimator):
+
+            # 构建弱分类器
+            model = DecisionTreeClassifier(max_depth=1)
+            model.fit(self.X, self.y, sample_weight=D)
+            predict = model.predict(self.X)
+
+            self.models.append(model)
+
+            # 计算错误率
+            err = np.sum(predict != self.y) / n_sample
+
+            # 计算弱分类器权重
+            alpha = np.log((1 - err) / err) / 2
+
+            self.alphas.append(alpha)
+
+            # 调整权重
+            D[predict != self.y] *= np.exp(alpha)
+            D[predict == self.y] *= np.exp(-alpha)
+            D /= np.sum(D)
+
+    def predict(self, X):
+        predictions = np.array([model.predict(X) for model in self.models])
+        weighted_predictions = []
+        for label in self.label:
+            temp_predict = (predictions == label).astype(int)
+            weighted_predictions.append(np.dot(self.alphas, temp_predict))
+        result = np.argmax(weighted_predictions, axis=0)
+        return result
+
+
+dataset = load_iris()
+X, y = dataset["data"], dataset["target"]
+X_train, X_test, y_train, y_test = train_test_split(X,
+                                                    y,
+                                                    test_size=0.2,
+                                                    random_state=23)
+
+adaboostModel = AdaBoost(X_train, y_train, 5)
+adaboostModel.fit()
+prediction = adaboostModel.predict(X_test)
+print(accuracy_score(prediction, y_test))
